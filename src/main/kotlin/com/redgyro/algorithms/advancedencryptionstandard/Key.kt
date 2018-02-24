59e37b9afb0928ef354550f86d0bacf53ac286fb
Johan Vergeer <johanvergeer@gmail.com>
2018-02-24 13:55:23 +0100

    Implement key expansion

--- src/main/kotlin/com/redgyro/algorithms/advancedencryptionstandard/Key.kt
+++ src/main/kotlin/com/redgyro/algorithms/advancedencryptionstandard/Key.kt
@@ -2,11 +2,31 @@ package com.redgyro.algorithms.advancedencryptionstandard
 
 class Key(val words: List<Word>) : TypedMaxLengthMutableList<Word>(maxSize = 8) {
 
-    constructor(vararg words: Word): this(words.toList())
+    constructor(vararg words: Word) : this(words.toList())
 
     init {
         assert(words.size in listOf(4, 6, 8), { "Key can hold 4, 6 or 8 words. (128, 192 or 256 bytes)" })
 
         this.innerList.addAll(this.words)
     }
+
+    override fun toString(): String {
+        return "Key:\n${words[0]}\n" +
+                "${words[1]}\n" +
+                "${words[2]}\n" +
+                "${words[3]}"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other is Key && this.innerList == other.innerList) return true
+
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return words.hashCode()
+    }
+
+
 }
---
+++ src/test/kotlin/com/redgyro/algorithms/advancedencryptionstandard/EncryptorTest.kt
@@ -0,0 +1,68 @@
+package com.redgyro.algorithms.advancedencryptionstandard
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+
+class EncryptorTest {
+    lateinit var key: Key
+    lateinit var firstExpandedKeyExpectation: Key
+    lateinit var lastExpandedKeyExpectation: Key
+
+    lateinit var encryptor: Encryptor
+
+    @Before
+    fun `setup`() {
+        this.key = Key(
+                Word(0x2B, 0x7E, 0x15, 0x16),
+                Word(0x28, 0xAE, 0xD2, 0xA6),
+                Word(0xAB, 0xF7, 0x15, 0x88),
+                Word(0x09, 0xCF, 0x4F, 0x3C)
+        )
+        this.firstExpandedKeyExpectation = Key(
+                Word(0xA0, 0xFA, 0xFE, 0x17),
+                Word(0x88, 0x54, 0x2C, 0xB1),
+                Word(0x23, 0xA3, 0x39, 0x39),
+                Word(0x2A, 0x6C, 0x76, 0x05))
+
+        this.lastExpandedKeyExpectation = Key(
+                Word(0xD0, 0x14, 0xF9, 0xA8),
+                Word(0xC9, 0xEE, 0x25, 0x89),
+                Word(0xE1, 0x3F, 0x0C, 0xC8),
+                Word(0xB6, 0x63, 0x0C, 0xA6))
+
+        this.encryptor = Encryptor("", this.key)
+    }
+
+    @Test
+    fun `Create list containing expanded keys`() {
+        // The list should have a length of 11 keys.
+
+        this.encryptor.expandKeys()
+
+        assertEquals(11, this.encryptor.keys.size)
+        assertEquals(this.firstExpandedKeyExpectation, this.encryptor.keys[1])
+        assertEquals(this.lastExpandedKeyExpectation, this.encryptor.keys[10])
+    }
+
+    @Test
+    fun `Create the first expanded key`() {
+
+        val expandedKey = encryptor.expandKey(1, this.key)
+
+        assertEquals(this.firstExpandedKeyExpectation, expandedKey)
+    }
+
+    @Test
+    fun `Create 10 expanded keys and test the last one`() {
+
+        var lastKey = this.key
+        for (round in 1..10) {
+            lastKey = encryptor.expandKey(round, lastKey)
+        }
+
+
+        assertEquals(this.lastExpandedKeyExpectation, lastKey)
+    }
+}
